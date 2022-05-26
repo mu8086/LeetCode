ccb4ee8dd8d89cadcf78cbdab59a1dbae138a3b6
mu8086 <mu8086@gmail.com>
2022-05-26 12:58:23 +0800

    1137. N-th Tribonacci Number - Loop

--- 1137. N-th Tribonacci Number/1137.c
+++ 1137. N-th Tribonacci Number/1137.c
@@ -1,8 +1,11 @@
 int tribonacci(int n){
-    static int arr[38] = {0, 1, 1};
+    static int arr[38] = {0, 1, 1, 2};
     
-    if (n >= 3 && arr[n] == 0) {
-        arr[n] = tribonacci(n-1) + tribonacci(n-2) + tribonacci(n-3);
+    if (n >= 4 && arr[n] == 0) {
+        for (int i=4, count=2; i<38; ++i) {
+            count += arr[i-1] - arr[i-4];
+            arr[i] = count;
+        }
     }
     
     return arr[n];
