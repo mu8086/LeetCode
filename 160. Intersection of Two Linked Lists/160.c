c5e22d86d4f3db06fc2e8ea3a213c6cad59070e6
mu8086 <mu8086@gmail.com>
2022-06-06 20:24:20 +0800

    160. Intersection of Two Linked Lists
    Traverse then right align

--- 160. Intersection of Two Linked Lists/160.c
+++ 160. Intersection of Two Linked Lists/160.c
@@ -1,45 +1,33 @@
-struct hash_entry {
-    void *key;
-    UT_hash_handle hh;
-};
-
-struct hash_entry *hash = NULL;
-
-void add_node(struct hash_entry *entry) {
-    HASH_ADD_PTR(hash, key, entry);
-}
-
-struct hash_entry *find_node(struct ListNode *node) {
-    struct hash_entry *ret;
-    HASH_FIND_PTR(hash, &node, ret);
-    return ret;
-}
-
-void delete_all() {
-    struct hash_entry *current, *tmp;
-
-    HASH_ITER(hh, hash, current, tmp) {
-        HASH_DEL(hash, current);
-        free(current);
-    }
-}
-
 struct ListNode *getIntersectionNode(struct ListNode *headA, struct ListNode *headB) {
-    while (headA != NULL) {
-        struct hash_entry *entry = (struct hash_entry *) malloc(sizeof(struct hash_entry));
-        entry->key = headA;
-        add_node(entry);
+    int sizeA, sizeB;
+    struct ListNode *tmp, *A, *B;
+    
+    for (sizeA = 0, A = headA; A != NULL; ++sizeA) {
+        A = A->next;
+    }
+    
+    for (sizeB = 0, B = headB; B != NULL; ++sizeB) {
+        B = B->next;
+    }
+
+    if (sizeA < sizeB) {
+        sizeA   = sizeB - sizeA;
+        
+        tmp     = headA;
+        headA   = headB;
+        headB   = tmp;
+    } else {
+        sizeA   = sizeA - sizeB;
+    }
+    
+    while (--sizeA >= 0) {
         headA = headA->next;
     }
     
-    while (headB != NULL) {
-        if (find_node(headB) != NULL) {
-            delete_all();
-            return headB;
-        }
+    while (headA != headB) {
+        headA = headA->next;
         headB = headB->next;
     }
-
-    delete_all();
-    return NULL;
+    
+    return headA;
 }
