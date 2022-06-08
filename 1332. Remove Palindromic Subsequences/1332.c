71fcc26bd37fdf3cab43bc00d0b055b90982dbbe
mu8086 <mu8086@gmail.com>
2022-06-08 11:26:04 +0800

    1332. Remove Palindromic Subsequences

--- 1332. Remove Palindromic Subsequences/1332.c
+++ 1332. Remove Palindromic Subsequences/1332.c
@@ -1,22 +1,9 @@
-bool isPalindrome(char *s, int len_s) {
-    for (int i=0, half=len_s/2, max_i=len_s-1; i<half; ++i) {
-        if (s[i] != s[max_i-i]) {
-            return false;
+int removePalindromeSub(char * s) {  
+    for (int i=0, j=strlen(s)-1; i<j; ++i, --j) {
+        if (s[i] != s[j]) {
+            return 2;
         }
     }
-    return true;
-}
 
-int removePalindromeSub(char * s) {
-    int len_s = strlen(s);
-    
-    if ( !isPalindrome(s, len_s) ) {
-        while (--len_s > 0) {
-            if ((s[len_s] + s[len_s-1]) % 2 == 1) {
-                return 2;
-            }
-        }
-    }
-    
     return 1;
 }
