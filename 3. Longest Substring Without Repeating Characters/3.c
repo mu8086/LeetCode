f73076000086e6153e738aba31710cbf3a490a60
mu8086 <mu8086@gmail.com>
2022-06-10 14:16:33 +0800

    3. Longest Substring Without Repeating Characters

--- 3. Longest Substring Without Repeating Characters/3.c
+++ 3. Longest Substring Without Repeating Characters/3.c
@@ -1,33 +1,19 @@
-int lengthOfLongestSubstring(char * s){
-    int index[256] = {0};
-    int i, max_len, curr_len, head_index;
-    
-    if (s[0] == '\0') {
-        return 0;
-    } else if (s[1] == '\0') {
-        return 1;
-    }
-    
-    for (i=1, max_len=1, head_index=0; s[i] != '\0'; i++) {
-        if (index[s[i]] == 0 && s[0] != s[i]) {
-            // first met this char in whole string s
-            index[s[i]] = i;
-        } else if (index[s[i]] < head_index) {
-            // first met this char in this substring
-            index[s[i]] = i;
-        } else {
-            // found the same char in this substring
-            curr_len = i - head_index;
-            if (max_len < curr_len)
-                max_len = curr_len;
-            
-            head_index = index[s[i]] + 1;
-            index[s[i]] = i;
+int lengthOfLongestSubstring(char * s) {
+    int begin, end, max, len;
+    int lastIndex[CHAR_MAX] = {};
+
+    for (begin = end = max = 0; s[end] != '\0'; ++end) {
+        // found a new non-repeated character
+        if (lastIndex[s[end]] >= begin) {
+            len = end - begin;
+            max = (len > max) ? len : max;
+            begin = lastIndex[s[end]];
         }
+        // indicate the new begin, if found this character later
+        lastIndex[s[end]] = end+1;
     }
-    curr_len = i - head_index;
-    if (max_len < curr_len)
-        max_len = curr_len;
-    
-    return max_len;
+    len = end - begin;
+    max = (len > max) ? len : max;
+
+    return max;
 }
