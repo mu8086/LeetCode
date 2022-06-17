0f591fd5452498258d780f81eec0d550e9608cf5
mu8086 <mu8086@gmail.com>
2022-06-17 12:26:16 +0800

    968. Binary Tree Cameras

---
+++ 968. Binary Tree Cameras/968.c
@@ -0,0 +1,40 @@
+#define ONE     1
+#define TWO     2
+#define THREE   0
+
+static inline int getType(int val) {
+    return (val != 0) ? val % 3 : 0;
+}
+
+static inline int getCamera(int val) {
+    return (val + 1) / 3;
+}
+
+int _minCameraCover(struct TreeNode* root) {
+    if (root == NULL) {
+        return 0;
+    } else {
+        int left, right, lType, rType;
+        
+        left    = _minCameraCover(root->left);
+        right   = _minCameraCover(root->right);
+        
+        lType = getType(left);
+        rType = getType(right);
+        
+        if (lType == ONE || rType == ONE) {
+            root->val = (getCamera(left) + getCamera(right)) * 3 + TWO;
+        } else if (lType == TWO || rType == TWO) {
+            root->val = (getCamera(left) + getCamera(right)) * 3 + THREE;
+        } else {
+            root->val = (getCamera(left) + getCamera(right)) * 3 + ONE;
+        }
+    }
+    
+    return root->val;
+}
+
+int minCameraCover(struct TreeNode* root) {
+    int rootVal = _minCameraCover(root);
+    return (getType(rootVal) == ONE) ? getCamera(rootVal)+1 : getCamera(rootVal);
+}
