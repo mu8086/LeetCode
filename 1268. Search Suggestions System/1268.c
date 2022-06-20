0f6ce7a90458d7c744010fc05a4557e80ac7444c
mu8086 <mu8086@gmail.com>
2022-06-20 09:28:24 +0800

    1268. Search Suggestions System
    [Speed Up]
    1. static variables for newNode()
    2. use last match result

--- 1268. Search Suggestions System/1268.c
+++ 1268. Search Suggestions System/1268.c
@@ -1,16 +1,27 @@
 #define ALPHABET_SIZE   26
 #define INVALID_INDEX   -1
 #define SUGGEST_SIZE    3
+#define MAX_NODE_SIZE   20000
 
 typedef struct _Node {
     int index;
     struct _Node *next[ALPHABET_SIZE];
 } Node;
 
-Node * newNode(void) {
-    Node *ret = (Node *) malloc(sizeof(Node));
-    memset(ret, 0, sizeof (Node));
-    ret->index = INVALID_INDEX;
+Node * newNode(int mode) {
+    static Node allocate[MAX_NODE_SIZE] = {};
+    static Node *ret = allocate;
+    
+    if (mode == 0) { // first call newNode()
+        memset(ret, 0, sizeof(Node) * MAX_NODE_SIZE);
+        for (int i = 0; i < MAX_NODE_SIZE; ++i) {
+            ret[i].index = INVALID_INDEX;
+        }
+    } else if (mode == 1) { // normal case
+        ++ret;
+    } else {    // do free
+        ret = allocate;
+    }
     
     return ret;
 }
@@ -23,12 +34,12 @@ void insertNode(Node *trie, char *s, int arrayIndex) {
     int index = getIndex(s[0]);
     
     if (trie->next[index] == NULL) {
-        trie->next[index] = newNode();
+        trie->next[index] = newNode(1);
     }
     
     if (s[1] != '\0') {
         insertNode(trie->next[index], s + 1, arrayIndex);
-    } else {
+    } else {    // last character in string
         trie->next[index]->index = arrayIndex;
     }
 }
@@ -47,19 +58,29 @@ void searchWords(Node *trie, int match[], int *matchCount) {
     }
 }
 
-void freeTrie(Node *trie) {
-    for (int i = 0; i < ALPHABET_SIZE; ++i) {
-        if (trie->next[i] != NULL) {
-            freeTrie(trie->next[i]);
+bool isLastMatchUseable(char c, int index, char **s, int match[], int matchCount) {
+    if (matchCount == 0) {
+        return false;
+    }
+    
+    for (int i = 0; i < matchCount; ++i) {
+        if (index >= strlen(s[match[i]]) || c != s[match[i]][index]) {
+            return false;
         }
     }
-    free(trie);
+
+    return true;
+}
+
+void freeTrie(Node *trie) {
+    newNode(-1);    // reset the static variable inside function
 }
 
 char *** suggestedProducts(char **products, int productsSize, char *searchWord, int *returnSize, int **returnColumnSizes) {
-    int i, j, index, len_search = strlen(searchWord), matchCount = 0;
+    int i, j, index, len_search = strlen(searchWord);
+    int matchCount = 0, matchSize = sizeof(int) * SUGGEST_SIZE, match[SUGGEST_SIZE] = {};
     char ***ret = (char ***) malloc(sizeof(char **) * len_search);
-    Node *trie = newNode(), *walker;
+    Node *trie = newNode(0), *walker;
 
     *returnSize = len_search;
     *returnColumnSizes = (int *) malloc(sizeof(int) * len_search);
@@ -69,14 +90,13 @@ char *** suggestedProducts(char **products, int productsSize, char *searchWord, 
     }  
     
     for (i = 0, walker = trie; i < len_search; ++i) {
-        matchCount = 0;
-
         if (walker != NULL) {
-            int match[SUGGEST_SIZE] = {};
-            
             index = getIndex(searchWord[i]);
-            
-            searchWords(walker->next[index], match, &matchCount);
+            if (!isLastMatchUseable(searchWord[i], i, products, match, matchCount)) {
+                matchCount = 0;
+                memset(match, 0, matchSize);
+                searchWords(walker->next[index], match, &matchCount);
+            }
             
             if (matchCount > 0) {
                 ret[i] = (char **) malloc(sizeof(char *) * matchCount);
@@ -86,8 +106,10 @@ char *** suggestedProducts(char **products, int productsSize, char *searchWord, 
             }
 
             walker = walker->next[index];
+        } else {
+            matchCount = 0;
         }
-        
+
         (*returnColumnSizes)[i] = matchCount;
     }
     
