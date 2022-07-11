ada2067e45ddb77a060706f7a5271554e355ed10
mu8086 <mu8086@gmail.com>
2022-07-11 21:43:27 +0800

    199. Binary Tree Right Side View

---
+++ 00199. Binary Tree Right Side View/00199.c
@@ -0,0 +1,33 @@
+#define MAX_NODE_DEPTH  100
+#define INIT_VAL        101
+
+void traversal(struct TreeNode *root, int depth, char tmp[]) {
+    if (root == NULL) {
+        return;
+    }
+    
+    if (tmp[depth] == INIT_VAL) {
+        tmp[depth] = root->val;
+    }
+    
+    traversal(root->right, depth+1, tmp);
+    traversal(root->left,  depth+1, tmp);
+}
+
+int * rightSideView(struct TreeNode *root, int *returnSize) {
+    static int ret[MAX_NODE_DEPTH] = {};
+    char tmp[MAX_NODE_DEPTH] = {};
+    memset(ret, 0, sizeof(int) * MAX_NODE_DEPTH);
+    memset(tmp, INIT_VAL, sizeof(char) * MAX_NODE_DEPTH);
+    
+    traversal(root, 0, tmp);
+
+    int i;
+    
+    for (i = 0; i < MAX_NODE_DEPTH && tmp[i] != INIT_VAL; ++i) {
+        ret[i] = (int)(tmp[i]);
+    }
+    *returnSize = i;
+    
+    return ret;
+}
