53b31c7e974833666e96940ff4209d58def3856f
mu8086 <mu8086@gmail.com>
2022-07-18 11:04:10 +0800

    328. Odd Even Linked List

---
+++ 00328. Odd Even Linked List/00328.c
@@ -0,0 +1,27 @@
+struct ListNode * oddEvenList(struct ListNode *head) {
+    if (head == NULL || head->next == NULL) {
+        return head;
+    }
+    
+    int i;
+    struct ListNode *walker = NULL, *odd = NULL, *even = NULL;
+    struct ListNode *evenHead = head->next;
+    
+    for (walker = head, i = 1; walker != NULL; ++i) {
+        if (i & 1) {
+            odd = walker;
+            walker = walker->next;
+        } else {
+            even = walker;
+            walker = walker->next;
+            
+            odd->next = even->next;
+            if (even->next != NULL) {
+                even->next = even->next->next;
+            }
+        }
+    }
+    odd->next = evenHead;
+    
+    return head;
+}
