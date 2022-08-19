c1b3992d890bb6c5e1ead19e7656b15f3fb77af2
mu8086 <mu8086@gmail.com>
2022-08-19 14:49:05 +0800

    2. Add Two Numbers - implement in java

--- 00002. Add Two Numbers/00002.java
+++ 00002. Add Two Numbers/00002.java
@@ -1,31 +1,31 @@
-struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2){
-    struct ListNode* result = l2;
-    
-    while (l1->next != NULL && l2->next != NULL) {
-        l2->val += l1->val;
-        if (l2->val > 9) {
-            l2->val -= 10;
-            l2->next->val += 1;
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode result = l2;
+        
+        while (l1.next != null && l2.next != null) {
+            l2.val += l1.val;
+            if (l2.val > 9) {
+                l2.val -= 10;
+                l2.next.val += 1;
+            }
+            l1 = l1.next;
+            l2 = l2.next;
         }
-        l1 = l1->next;
-        l2 = l2->next;
-    }
-    if (l2->next == NULL) {
-        l2->next = l1->next;
-    }
-    
-    l2->val += l1->val;
-    while (l2->val > 9) {
-        l2->val -= 10;
-        if (l2->next == NULL) {
-            l2->next = malloc(sizeof (struct ListNode));
-            l2->next->val = 1;
-            l2->next->next = NULL;
-            return result;
+        if (l2.next == null) {
+            l2.next = l1.next;
         }
-        l2->next->val += 1;
-        l2 = l2->next;
+        
+        l2.val += l1.val;
+        while (l2.val > 9) {
+            l2.val -= 10;
+            if (l2.next == null) {
+                l2.next = new ListNode(1);
+                return result;
+            }
+            l2.next.val += 1;
+            l2 = l2.next;
+        }
+        
+        return result;
     }
-    
-    return result;
 }
