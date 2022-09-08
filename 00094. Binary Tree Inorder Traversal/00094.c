96934d7ff0f8fed023b51f0e89aad1f623f3aefe
mu8086 <mu8086@gmail.com>
2022-09-08 08:43:46 +0800

    94. Binary Tree Inorder Traversal - Coding Style

--- 00094. Binary Tree Inorder Traversal/00094.c
+++ 00094. Binary Tree Inorder Traversal/00094.c
@@ -1,22 +1,23 @@
-void inorder(int *arr, int *index, struct TreeNode* root) {
-    if (root->left != NULL) {
+void inorder(int *arr, int *index, struct TreeNode *root) {
+    if (root != NULL) {
         inorder(arr, index, root->left);
-    }
-    arr[(*index)++] = root->val;
-    if (root->right != NULL) {
+
+        arr[(*index)++] = root->val;
+
         inorder(arr, index, root->right);
     }
 }
 
-int* inorderTraversal(struct TreeNode* root, int* returnSize) {
+int * inorderTraversal(struct TreeNode *root, int *returnSize) {
     *returnSize = 0;
-    if (root == NULL) {
-        return NULL;
+    
+    if (root != NULL) {
+        int *ret = (int *) malloc(sizeof(int) * 100);
+    
+        inorder(ret, returnSize, root);
+        
+        return ret;
     }
     
-    int *ret = (int *) malloc(sizeof(int) * 100);
-    
-    inorder(ret, returnSize, root);
-    
-    return ret;
+    return NULL;
 }
