5587f3e9952092cfe483cf776b616a2537325a25
mu8086 <mu8086@gmail.com>
2022-09-13 10:07:08 +0800

    393. UTF-8 Validation

--- 00393. UTF-8 Validation/00393.c
+++ 00393. UTF-8 Validation/00393.c
@@ -1,18 +1,18 @@
 int getContinuationSize(int byte) {
-    static int flag[4]   = {128, 224, 240, 248};
-    static int target[4] = {  0, 192, 224, 240};
-
+    static int flag[4]   = { 0b10000000, 0b11100000, 0b11110000, 0b11111000 };
+    static int target[4] = { 0b00000000, 0b11000000, 0b11100000, 0b11110000 };
+    
     for (int i = 0; i < 4; ++i) {
         if ((byte & flag[i]) == target[i]) {
             return i;
         }
     }
-
+    
     return -1;
 }
 
 bool verifyContinuation(int byte) {
-    return (byte & 192) == 128;
+    return (byte & 0b11000000) == 0b10000000;
 }
 
 bool validUtf8(int *data, int dataSize) {
@@ -20,13 +20,13 @@ bool validUtf8(int *data, int dataSize) {
         if ((n = getContinuationSize(data[i])) == -1 || i + n >= dataSize) {
             return false;
         }
-
+        
         for (j = i+1; j <= i+n; ++j) {
             if (verifyContinuation(data[j]) == false) {
                 return false;
             }
         }
     }
-
+    
     return true;
 }
