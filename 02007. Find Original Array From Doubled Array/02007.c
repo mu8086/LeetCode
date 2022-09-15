52eeb210d2c901cf6218aef7898ff8feb78a599d
mu8086 <mu8086@gmail.com>
2022-09-15 11:10:23 +0800

    2007. Find Original Array From Doubled Array - Binary Search

--- 02007. Find Original Array From Doubled Array/02007.c
+++ 02007. Find Original Array From Doubled Array/02007.c
@@ -1,8 +1,34 @@
 #define NOT_FOUND   -1
-#define MAX_SIZE    200001
 
 int compare(const void *a, const void *b) {
-    return *(int *)b - *(int *)a;
+    return *(int *)a - *(int *)b;
+}
+
+int searchIndex(int *changed, bool *visited, int begin, int end, int targetVal) {
+    int midIdx, midVal;
+    
+    while (begin <= end) {
+        midIdx = (begin + end) / 2;
+        midVal = changed[midIdx];
+        
+        if (midVal == targetVal) {
+            if (visited[midIdx] == false) {
+                if (midIdx+1 <= end && changed[midIdx+1] == targetVal && visited[midIdx+1] == false) {
+                    begin = midIdx+1;
+                    continue;
+                }
+                return midIdx;
+            } else {
+                end = midIdx-1;
+            }
+        } else if (midVal < targetVal) {
+            begin = midIdx+1;
+        } else {
+            end = midIdx-1;
+        }
+    }
+    
+    return NOT_FOUND;
 }
 
 int * findOriginalArray(int *changed, int changedSize, int *returnSize) {
@@ -12,42 +38,27 @@ int * findOriginalArray(int *changed, int changedSize, int *returnSize) {
     }
     
     *returnSize = changedSize / 2;
-    int i, retIdx, n;
     int *ret = (int *) malloc(sizeof(int) * *returnSize);
-    int freq[MAX_SIZE] = {};
+    
+    bool *visited = (bool *) malloc(sizeof(bool) * changedSize);
+    memset(visited, 0, sizeof(bool) * changedSize);
     
     qsort(changed, changedSize, sizeof(int), compare);
     
-    for (i = 0; i < changedSize; ++i) {
-        ++freq[changed[i]];
-    }
-    
-    for (i = changedSize-1, retIdx = 0; i >= 0; --i) {
-        n = changed[i];
-        
-        if (freq[n] > 0) {
-            if (freq[n << 1] == 0) {
-                *returnSize = 0;
+    for (int i = 0, retIdx = 0, lastI = changedSize-1, doubledIdx; i <= lastI; ++i) {
+        if (visited[i] == false) {
+            if ((doubledIdx = searchIndex(changed, visited, i+1, lastI, changed[i] * 2)) == NOT_FOUND) {
+                free(visited);
                 free(ret);
-                ret = NULL;
-                break;
+                *returnSize = 0;
+                return NULL;
             } else {
-                // corner case: freq[0*2] --> -1, but
-                // [1] changedSize must be even number
-                // [2] freq[changed[i]] must be paired
-                // so if it happens, there is a number meet the above 'if' statement
-                // then return NULL
-                --freq[n];
-                if (--freq[n << 1] < 0) {
-                    *returnSize = 0;
-                    free(ret);
-                    ret = NULL;
-                    break;
-                }
-                ret[retIdx++] = n;
+                ret[retIdx++] = changed[i];
+                visited[doubledIdx] = true;
             }
         }
     }
     
+    free(visited);
     return ret;
 }
