f386ca9c685d9c7e5e2c0de1b31aae921116d880
mu8086 <mu8086@gmail.com>
2022-09-28 09:33:37 +0800

    19. Remove Nth Node From End of List

--- 00019. Remove Nth Node From End of List/00019.c
+++ 00019. Remove Nth Node From End of List/00019.c
@@ -1,18 +1,22 @@
-struct ListNode* removeNthFromEnd(struct ListNode* head, int n){
-    int stack_index = 0;
-    struct ListNode* walker;
-    struct ListNode* stack[31] = {};
-    
-    for (walker=head; walker!=NULL; walker=walker->next) {
-        stack[stack_index++] = walker;
+// https://leetcode.com/problems/remove-nth-node-from-end-of-list/
+
+struct ListNode * removeNthFromEnd(struct ListNode * head, int n) {
+    struct ListNode *fast = head, *slow = NULL, *last = NULL;
+
+    for (int i = 0; i < n; ++i) {
+        fast = fast->next;
     }
-    if (n < stack_index) {
-        stack[stack_index-n-1]->next = stack[stack_index-n]->next;
-        free(stack[stack_index-n]);
-    } else if (n == stack_index) {
-        free(stack[0]);
-        return stack[1];
+
+    for (slow = head, last = NULL; fast != NULL; fast = fast->next, slow = slow->next) {
+        last = slow;
     }
-    
+
+    if (slow == head) {
+        head = head->next;
+    } else {
+        last->next = slow->next;
+    }
+
+    free(slow);
     return head;
 }
