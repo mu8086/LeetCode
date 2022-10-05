9c02de1749edf89bcf84016f49cc7a1bf3ff020d
mu8086 <mu8086@gmail.com>
2022-10-05 11:19:16 +0800

    623. Add One Row to Tree - other people's solution

--- 00623. Add One Row to Tree/00623.c
+++ 00623. Add One Row to Tree/00623.c
@@ -3,26 +3,19 @@ struct TreeNode * addOneRow(struct TreeNode *root, int val, int depth) {
         struct TreeNode *newNode = (struct TreeNode *) malloc(sizeof(struct TreeNode));
         *newNode = (struct TreeNode){ .left = root, .right = NULL, .val = val };
         return newNode;
-    } else if (root == NULL) {
-        return NULL;
+    } else if (depth == 2) {
+        struct TreeNode *nodes = (struct TreeNode *) malloc(sizeof(struct TreeNode) * 2);
+        *(nodes)    = (struct TreeNode){ .left = root->left,    .right = NULL,          .val = val          };
+        *(nodes+1)  = (struct TreeNode){ .left = NULL,          .right = root->right,   .val = val          };
+        *root       = (struct TreeNode){ .left = nodes,         .right = nodes+1,       .val = root->val    };
+        return root;
     }
 
-    struct TreeNode *l = addOneRow(root->left,  val, depth-1);
-    struct TreeNode *r = addOneRow(root->right, val, depth-1);
-
-    // if l is a newNode, connect root->left to it
-    if (l != root->left) {
-        root->left = l;
+    if (root->left != NULL) {
+        addOneRow(root->left, val, depth-1);
     }
-
-    // if r is a newNode, connect root->right to it
-    if (r != root->right) {
-        // for handle original depth 1 case (i.e. insert newNode as root's parent),
-        // we set the root as default newNode->left
-        // but in the r case, we should swap the newNode->left & newNode->right
-        r->left = NULL;
-        r->right = root->right;
-        root->right = r;
+    if (root->right != NULL) {
+        addOneRow(root->right, val, depth-1);
     }
 
     return root;
