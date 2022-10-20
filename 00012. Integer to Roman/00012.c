95c0c8e94a8c1d9ef601094ed32f0789493fc570
mu8086 <mu8086@gmail.com>
2022-10-20 08:14:25 +0800

    12. Integer to Roman
    https://leetcode.com/problems/integer-to-roman

--- 00012. Integer to Roman/00012.c
+++ 00012. Integer to Roman/00012.c
@@ -1,55 +1,64 @@
-char * intToRoman(int num){
-    char* ret = (char*) malloc(sizeof(char) * 20);
-    memset(ret, 0, sizeof(char) * 20);
-    
-    for (int i=0; num > 0; i++) {
-        if (num >= 1000) {
-            num -= 1000;
-            ret[i] = 'M';
-        } else if (num >= 900) {
-            num -= 900;
-            ret[i] = 'C';
-            i++, ret[i] = 'M';
-        } else if (num >= 500) {
-            num -= 500;
-            ret[i] = 'D';
-        } else if (num >= 400) {
-            num -= 400;
-            ret[i] = 'C';
-            i++, ret[i] = 'D';
-        } else if (num >= 100) {
-            num -= 100;
-            ret[i] = 'C';
-        } else if (num >= 90) {
-            num -= 90;
-            ret[i] = 'X';
-            i++, ret[i] = 'C';
-        } else if (num >= 50) {
-            num -= 50;
-            ret[i] = 'L';
-        } else if (num >= 40) {
-            num -= 40;
-            ret[i] = 'X';
-            i++, ret[i] = 'L';
-        } else if (num >= 10) {
-            num -= 10;
-            ret[i] = 'X';
-        } else if (num >= 9) {
-            num -= 9;
-            ret[i] = 'I';
-            i++, ret[i] = 'X';
-        } else if (num >= 5) {
-            num -= 5;
-            ret[i] = 'V';
-        } else if (num >= 4) {
-            num -= 4;
-            ret[i] = 'I';
-            i++, ret[i] = 'V';
-        } else if (num >= 1) {
-            num -= 1;
-            ret[i] = 'I';
-        }
+// https://leetcode.com/problems/integer-to-roman
+
+char * intToRoman(int num) {
+    int i;
+    char s[20] = {}, *ans = NULL;
+
+    i = 0;
+    while (num >= 1000) {
+        num -= 1000;
+        s[i++] = 'M';
     }
-    
-    return ret;
+    if (num >= 900) {
+        num -= 900;
+        s[i++] = 'C';
+        s[i++] = 'M';
+    } else if (num >= 500) {
+        num -= 500;
+        s[i++] = 'D';
+    } else if (num >= 400) {
+        num -= 400;
+        s[i++] = 'C';
+        s[i++] = 'D';
+    }
+    while (num >= 100) {
+        num -= 100;
+        s[i++] = 'C';
+    }
+    if (num >= 90) {
+        num -= 90;
+        s[i++] = 'X';
+        s[i++] = 'C';
+    } else if (num >= 50) {
+        num -= 50;
+        s[i++] = 'L';
+    } else if (num >= 40) {
+        num -= 40;
+        s[i++] = 'X';
+        s[i++] = 'L';
+    }
+    while (num >= 10) {
+        num -= 10;
+        s[i++] = 'X';
+    }
+    if (num >= 9) {
+        num -= 9;
+        s[i++] = 'I';
+        s[i++] = 'X';
+    } else if (num >= 5) {
+        num -= 5;
+        s[i++] = 'V';
+    } else if (num >= 4) {
+        num -= 4;
+        s[i++] = 'I';
+        s[i++] = 'V';
+    }
+    while (num >= 1) {
+        num -= 1;
+        s[i++] = 'I';
+    }
+
+    ans = (char *) malloc(sizeof(char) * (i+1));
+    memcpy(ans, s, sizeof(char) * (i+1));
+    return ans;
 }
