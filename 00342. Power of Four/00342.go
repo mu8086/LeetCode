92664b43d8082b041c17ba9ca5534393491390d3
mu8086 <mu8086@gmail.com>
2023-10-23 13:15:16 +0800

    342. Power of Four
    https://leetcode.com/problems/power-of-four
    
    (Submissions from others)

--- 00342. Power of Four/00342.go
+++ 00342. Power of Four/00342.go
@@ -1,13 +1,16 @@
 // https://leetcode.com/problems/power-of-four
 
 func isPowerOfFour(n int) bool {
+    // if n is zero or negative, it's not a power of 4.
     if n <= 0 {
         return false
     }
 
-    for (n & 3) == 0 {
-        n >>= 2
+    // check if n has only one '1' bit (i.e., it's a power of two).
+    if n & (n-1) != 0 {
+        return false
     }
 
-    return n == 1
+    // only powers of 4, within the powers of 2 set, have a remainder of 1 when divided by 3.
+    return n % 3 == 1
 }
