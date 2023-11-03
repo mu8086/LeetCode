e7fe38a8c5ae3da506bf073dd542fcfcc365efe8
mu8086 <mu8086@gmail.com>
2023-11-03 08:33:48 +0800

    2433. Find The Original Array of Prefix Xor
    https://leetcode.com/problems/find-the-original-array-of-prefix-xor

---
+++ 02433. Find The Original Array of Prefix Xor/02433.go
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/find-the-original-array-of-prefix-xor
+
+func findArray(pref []int) []int {
+    ans := make([]int, len(pref), len(pref))
+    ans[0] = pref[0]
+    for i := 1; i < len(pref); i++ {
+        ans[i] = pref[i] ^ pref[i-1]
+    }
+    return ans
+}
