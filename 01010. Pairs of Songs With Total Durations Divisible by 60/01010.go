d896daa977fb4a77fe8c26eef7e41075d7959766
mu8086 <mu8086@gmail.com>
2023-11-15 23:38:14 +0800

    1010. Pairs of Songs With Total Durations Divisible by 60
    https://leetcode.com/problems/pairs-of-songs-with-total-durations-divisible-by-60

--- 01010. Pairs of Songs With Total Durations Divisible by 60/01010.go
+++ 01010. Pairs of Songs With Total Durations Divisible by 60/01010.go
@@ -1,72 +1,13 @@
 // https://leetcode.com/problems/pairs-of-songs-with-total-durations-divisible-by-60
 
-// others' submission
-func numPairsDivisibleBy60(time []int) int {
-    total := 0
-    mods := make([]int, 60)
+func numPairsDivisibleBy60(time []int) (ans int) {
+    mods := [60]int{}
 
-    for i := 0; i < len(time); i++ {    
-         m := time[i] % 60
-        if m == 0  {
-            total += (mods[m])
-        } else {
-            total += (mods[60-m])
-        }
-        mods[m] += 1
+    for i := len(time)-1; i >= 0; i-- {
+        t := time[i] % 60
+        ans += mods[(60-t) % 60]
+        mods[t]++
     }
 
-    return total
-}
-
-// normal (based on sort & binarySearch)
-func numPairsDivisibleBy60Ver2(time []int) (pair int) {
-    size := len(time)
-    for i := 0; i < size; i++ {
-        time[i] %= 60
-    }
-
-    sort.Ints(time)
-
-    zeroSize := sort.SearchInts(time, 1)
-    pair += zeroSize * (zeroSize - 1) / 2
-    if zeroSize == size {
-        return pair
-    }
-
-    thirtyIdx := sort.SearchInts(time, 30)
-    if thirtyIdx != size {
-        thirtySize := sort.SearchInts(time, 31) - thirtyIdx
-        pair += thirtySize * (thirtySize-1) / 2
-    }
-
-    for val := 1; val < 30; val++ {
-        myIdx := sort.SearchInts(time, val)
-        if myIdx == size || time[myIdx] != val {
-            continue
-        }
-
-        otherIdx := sort.SearchInts(time, 60-val)
-        if otherIdx == size || time[otherIdx] != 60-val {
-            continue
-        }
-
-        mySize := sort.SearchInts(time, val+1) - myIdx
-        otherSize := sort.SearchInts(time, 60-val+1) - otherIdx
-
-        pair += mySize*otherSize
-    }
-
-    return pair
-}
-
-// bad
-func numPairsDivisibleBy60Ver1(time []int) (pair int) {
-    for i, lastIdx := 0, len(time)-1; i < lastIdx; i++ {
-        for j := i+1; j <= lastIdx; j++ {
-            if (time[i] + time[j]) % 60 == 0 {
-                pair++
-            }
-        }
-    }
-    return pair
+    return ans
 }
