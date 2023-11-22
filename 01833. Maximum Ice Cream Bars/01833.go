765cf3856b67f633fc018a0fe60d18dbf0dbbc5f
mu8086 <mu8086@gmail.com>
2023-11-22 12:31:26 +0800

    1833. Maximum Ice Cream Bars
    https://leetcode.com/problems/maximum-ice-cream-bars

--- 01833. Maximum Ice Cream Bars/01833.go
+++ 01833. Maximum Ice Cream Bars/01833.go
@@ -1,10 +1,12 @@
 // https://leetcode.com/problems/maximum-ice-cream-bars
 
 func maxIceCream(costs []int, coins int) (ans int) {
+    size := len(costs)
     count := make([]int, 100001, 100001)
 
-    min, max := costs[0], costs[0]
+    sum, min, max := 0, costs[0], costs[0]
     for _, price := range costs {
+        sum += price
         count[price]++
 
         if min > price {
@@ -14,16 +16,20 @@ func maxIceCream(costs []int, coins int) (ans int) {
             max = price
         }
     }
+    if coins >= sum {
+        return size
+    }
 
     for price := min; price <= max; price++ {
         amount := count[price]
-        if amount == 0 {
+        if amount == 0 || size == 0 {
             continue
         }
 
         if tmp := amount * price; coins > tmp {
             coins -= tmp
             ans += amount
+            size -= amount
         } else {
             ans += coins / price
             break
