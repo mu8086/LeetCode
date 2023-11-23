4036252a5efadf60c1e21da4d406042d2f8a2fb3
mu8086 <mu8086@gmail.com>
2023-11-23 09:50:16 +0800

    1630. Arithmetic Subarrays
    https://leetcode.com/problems/arithmetic-subarrays

--- 01630. Arithmetic Subarrays/01630.go
+++ 01630. Arithmetic Subarrays/01630.go
@@ -1,25 +1,43 @@
 // https://leetcode.com/problems/arithmetic-subarrays
 
-func checkArithmeticSubarrays(nums []int, l []int, r []int) []bool {
-    isArithmetic := func(arr []int) bool {
-        for i, diff := len(arr)-1, arr[1] - arr[0]; i > 0; i-- {
-            if arr[i]-arr[i-1] != diff {
-                return false
-            }
-        }
+func isArithmetic(nums []int, l, r, query int, m map[int]int) bool {
+    size := r - l + 1
+    if size <= 2 {
         return true
     }
 
+    min, max := nums[l], nums[l]
+    for i := l; i <= r; i++ {
+        m[nums[i]] = query
+
+        if min > nums[i] {
+            min = nums[i]
+        }
+        if max < nums[i] {
+            max = nums[i]
+        }
+    }
+
+    if (max - min) % (size - 1) != 0 {
+        return false
+    }
+
+    for tmp, diff := min, (max-min) / (size-1); size > 0; tmp, size = tmp+diff, size-1 {
+        if val, exists := m[tmp]; !exists || val != query {
+            return false
+        }
+    }
+
+    return true
+}
+
+func checkArithmeticSubarrays(nums []int, l []int, r []int) []bool {
     size := len(l)
     ans := make([]bool, size, size)
+    m := make(map[int]int)  // key: nums[i], value: lastest occurrence in l[i] ~ r[i]
+
     for i := 0; i < size; i++ {
-        subArr := make([]int, r[i]+1-l[i], r[i]+1-l[i])
-
-        copy(subArr, nums[l[i]:r[i]+1])
-
-        sort.Ints(subArr)
-
-        ans[i] = isArithmetic(subArr)
+        ans[i] = isArithmetic(nums, l[i], r[i], i, m)
     }
 
     return ans
