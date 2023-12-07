d5691918be39e3f1b99f1ab970b820c7e10cc199
mu8086 <mu8086@gmail.com>
2023-12-07 09:33:35 +0800

    1903. Largest Odd Number in String
    https://leetcode.com/problems/largest-odd-number-in-string

---
+++ 01903. Largest Odd Number in String/01903.go
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/largest-odd-number-in-string
+
+func largestOddNumber(num string) string {
+    for i := len(num)-1; i >= 0; i-- {
+        if (num[i] & 1) == 1 {
+            return num[:i+1]
+        }
+    }
+    return ""
+}
