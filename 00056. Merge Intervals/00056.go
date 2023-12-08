44ac3bcae6b7cfb59ca032fa8d57561c0becafce
mu8086 <mu8086@gmail.com>
2023-12-08 15:18:54 +0800

    56. Merge Intervals
    https://leetcode.com/problems/merge-intervals

--- 00056. Merge Intervals/00056.go
+++ 00056. Merge Intervals/00056.go
@@ -8,19 +8,19 @@ func merge(intervals [][]int) (ans [][]int) {
 
     size := len(intervals)
 
-    headIdx, farest := 0, intervals[0][1]
+    head, farest := intervals[0][0], intervals[0][1]
     for i := 1; i < size; i++ {
         if farest >= intervals[i][0] {
             if farest < intervals[i][1] {
                 farest = intervals[i][1]
             }
         } else {
-            ans = append(ans, []int{intervals[headIdx][0], farest})
-            headIdx, farest = i, intervals[i][1]
+            ans = append(ans, []int{head, farest})
+            head, farest = intervals[i][0], intervals[i][1]
         }
     }
 
-    ans = append(ans, []int{intervals[headIdx][0], farest})
+    ans = append(ans, []int{head, farest})
 
     return ans
 }
