7c28dc29b15711e48adb789f7979c110aa424eb1
mu8086 <mu8086@gmail.com>
2024-01-13 08:09:43 +0800

    1347. Minimum Number of Steps to Make Two Strings Anagram
    https://leetcode.com/problems/minimum-number-of-steps-to-make-two-strings-anagram

---
+++ 01347. Minimum Number of Steps to Make Two Strings Anagram/01347.go
@@ -0,0 +1,20 @@
+// https://leetcode.com/problems/minimum-number-of-steps-to-make-two-strings-anagram
+
+func minSteps(s string, t string) (ans int) {
+    m := make(map[rune]int)
+
+    for _, c := range s {
+        m[c]++
+    }
+    for _, c := range t {
+        m[c]--
+    }
+
+    for _, v := range m {
+        if v > 0 {
+            ans += v
+        }
+    }
+
+    return ans
+}
