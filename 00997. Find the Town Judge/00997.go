52a3fd249efbc1342af0ddace5e799dcb6680d0f
mu8086 <mu8086@gmail.com>
2024-02-22 09:27:45 +0800

    997. Find the Town Judge
    https://leetcode.com/problems/find-the-town-judge

--- 00997. Find the Town Judge/00997.go
+++ 00997. Find the Town Judge/00997.go
@@ -1,26 +1,23 @@
 // https://leetcode.com/problems/find-the-town-judge
 
 func findJudge(n int, trust [][]int) int {
-    size := len(trust)
     trustAnother := make([]bool, n+1, n+1)
     trustByAnother := make([]int, n+1, n+1)
 
-    for i := 0; i < size; i++ {
+    judge := 1
+    maxTrustByAnother := 0
+    for i, size := 0, len(trust); i < size; i++ {
         trustAnother[trust[i][0]] = true
         trustByAnother[trust[i][1]]++
-    }
 
-    judge, threshold := -1, n-1
-    for i := 1; i <= n; i++ {
-        if trustByAnother[i] == threshold && trustAnother[i] == false {
-            if judge == -1 {
-                judge = i
-            } else {
-                judge = -1
-                break
-            }
+        if maxTrustByAnother < trustByAnother[trust[i][1]] {
+            judge = trust[i][1]
+            maxTrustByAnother = trustByAnother[trust[i][1]]
         }
     }
 
-    return judge
+    if trustAnother[judge] == false && trustByAnother[judge] == n-1 {
+        return judge
+    }
+    return -1
 }
