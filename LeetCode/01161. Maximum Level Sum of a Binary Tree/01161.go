0115f3c6cbdda27ad33f85a5b48e309a54f89163
mu8086 <mu8086@gmail.com>
2026-01-06 09:57:28 +0800

    1161. Maximum Level Sum of a Binary Tree
    https://leetcode.com/problems/maximum-level-sum-of-a-binary-tree

--- LeetCode/01161. Maximum Level Sum of a Binary Tree/01161.go
+++ LeetCode/01161. Maximum Level Sum of a Binary Tree/01161.go
@@ -27,3 +27,32 @@ func maxLevelSum(root *TreeNode) int {
 
     return maxLevel
 }
+
+func helper2(node *TreeNode, level int, sum map[int]int) {
+    if node == nil {
+        return
+    }
+
+    sum[level] += node.Val
+
+    helper2(node.Left, level+1, sum)
+    helper2(node.Right, level+1, sum)
+}
+
+func maxLevelSum2(root *TreeNode) int {
+    sumMap := make(map[int]int)
+    helper2(root, 1, sumMap)
+
+    level := 1
+    maxSum := root.Val
+    for l, sum := range sumMap {
+        if maxSum < sum {
+            maxSum = sum
+            level = l
+        } else if maxSum == sum && level > l {
+            level = l
+        }
+    }
+
+    return level
+}
