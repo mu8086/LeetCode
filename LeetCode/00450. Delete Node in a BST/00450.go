b13221346df8b24ca3726c22eb03d7ce8cf11964
mu8086 <mu8086@gmail.com>
2026-02-09 14:07:04 +0800

    450. Delete Node in a BST
    https://leetcode.com/problems/delete-node-in-a-bst

--- LeetCode/00450. Delete Node in a BST/00450.go
+++ LeetCode/00450. Delete Node in a BST/00450.go
@@ -3,39 +3,27 @@
 func deleteNode(root *TreeNode, key int) *TreeNode {
     if root == nil {
         return nil
-    }
-
-    getReplaceNode := func(root *TreeNode) *TreeNode {
-        node := root.Right
-        for node != nil && node.Left != nil {
-            node = node.Left
-        }
-
-        if node == nil {
-            node = root.Left
-            for node != nil && node.Right != nil {
-                node = node.Right
-            }
-        }
-
-        return node
-    }
-
-    if root.Val > key {
-        root.Left = deleteNode(root.Left, key)
     } else if root.Val < key {
         root.Right = deleteNode(root.Right, key)
+    } else if root.Val > key {
+        root.Left = deleteNode(root.Left, key)
     } else {
-        replaceNode := getReplaceNode(root)
-
-        if replaceNode == nil {
-            root = nil
-        } else {
-            tmp := replaceNode.Val
-            root = deleteNode(root, tmp)
-            root.Val = tmp
+        if root.Right == nil {
+            return root.Left
+        } else if root.Left == nil {
+            return root.Right
         }
-    } 
+
+        root.Val = findMin(root.Right)
+        root.Right = deleteNode(root.Right, root.Val)
+    }
 
     return root
+}
+
+func findMin(node *TreeNode) int {
+    for node.Left != nil {
+        node = node.Left
+    }
+    return node.Val
 }
