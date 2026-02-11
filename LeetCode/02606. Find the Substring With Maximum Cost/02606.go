8462fa4d03ea48127cf12acfe6c60f690933f17e
mu8086 <mu8086@gmail.com>
2026-02-11 22:10:52 +0800

    2606. Find the Substring With Maximum Cost
    https://leetcode.com/problems/find-the-substring-with-maximum-cost

--- LeetCode/02606. Find the Substring With Maximum Cost/02606.go
+++ LeetCode/02606. Find the Substring With Maximum Cost/02606.go
@@ -1,29 +1,26 @@
 // https://leetcode.com/problems/find-the-substring-with-maximum-cost
 
-func maximumCostSubstring(s string, chars string, vals []int) int {
-    m := [26]int{}
-
+func maximumCostSubstring(s, chars string, vals []int) int {
+    val := [26]int{}
     for i := 0; i < 26; i++ {
-        m[i] = i+1
+        val[i] = i+1
+    }
+    for k, v := range chars {
+        val[v-'a'] = vals[k]
     }
 
-    for i, c := range chars {
-        m[c - 'a'] = vals[i]
-    }
-
-    max, count := 0, 0
-    for lIdx, rIdx, size := 0, 0, len(s); rIdx < size; rIdx++ {
-        count += m[s[rIdx] - 'a']
-
-        for count < 0 {
-            count -= m[s[lIdx] - 'a']
-            lIdx++
+    ans, prefix := 0, 0
+    for _, c := range s {
+        prefix += val[c-'a']
+        if prefix < 0 {
+            prefix = 0
+            continue
         }
 
-        if max < count {
-            max = count
+        if ans < prefix {
+            ans = prefix
         }
     }
 
-    return max
+    return ans
 }
