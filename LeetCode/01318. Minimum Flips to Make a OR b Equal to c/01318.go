4ddad28072537e2a39ad1f658c82e3201aaad2d9
mu8086 <mu8086@gmail.com>
2026-02-11 19:17:56 +0800

    1318. Minimum Flips to Make a OR b Equal to c
    https://leetcode.com/problems/minimum-flips-to-make-a-or-b-equal-to-c

--- LeetCode/01318. Minimum Flips to Make a OR b Equal to c/01318.go
+++ LeetCode/01318. Minimum Flips to Make a OR b Equal to c/01318.go
@@ -1,21 +1,27 @@
 // https://leetcode.com/problems/minimum-flips-to-make-a-or-b-equal-to-c
 
-func minFlips(a int, b int, c int) (ans int) {
-    for i, xor := 0, (a|b) ^ c; i < 32; i++ {
-        if ((xor >> i) & 1) == 0 {
-            continue
+func minFlips(a, b, c int) int {
+    xor := (a|b)^c
+
+    count := 0
+    for offset := 0; xor > 0; offset++ {
+        if (xor & 1) != 0 {
+            count += flipTimes(a, b, c, offset)
         }
 
-        if ((c >> i) & 1) == 0 {
-            if ((a >> i) & 1) == 1 {
-                ans++
-            }
-            if ((b >> i) & 1) == 1 {
-                ans++
-            }
-        } else {
-            ans++
-        }
+        xor >>= 1
     }
-    return ans
+
+    return count
+}
+
+func flipTimes(a, b, c, offset int) int {
+    a = (a >> offset) & 1
+    b = (b >> offset) & 1
+    c = (c >> offset) & 1
+
+    if c == 1 {
+        return 1
+    }
+    return a+b
 }
